fe121528d320ffffe6627f49142a4412629cc748
AroJacq <arojoseph1987@gmail.com>
2019-09-10 16:25:27 +0530

    Add tariff plan added

--- src/test/java/stepDefinition/AddCustomer.java
+++ src/test/java/stepDefinition/AddCustomer.java
@@ -1,10 +1,14 @@
 package stepDefinition;
 
+import java.util.List;
+import java.util.Map;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -13,13 +17,13 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class AddCustomer {
 
 	static WebDriver driver;
-	
+
 	@Given("The user launch the url")
 	public void the_user_launch_the_url() {
-	     WebDriverManager.chromedriver().setup();
-	      driver = new ChromeDriver();
-	  driver.get("http://demo.guru99.com/telecom/");
-	   driver.manage().window().maximize();
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("http://demo.guru99.com/telecom/");
+		driver.manage().window().maximize();
 	}
 
 	@Given("The user click the Add customer")
@@ -28,31 +32,73 @@ public class AddCustomer {
 	}
 
 	@When("The user fill the all details")
-	public void the_user_fill_the_all_details() {
-	    driver.findElement(By.xpath("//*[text()='Done']")).click();
-	    driver.findElement(By.id("fname")).sendKeys("Aro");
-	    driver.findElement(By.id("lname")).sendKeys("Jacq");
-	    driver.findElement(By.name("emailid")).sendKeys("arojacq@gmail.com");
-	    driver.findElement(By.name("addr")).sendKeys("Velachery");
-	    driver.findElement(By.name("telephoneno")).sendKeys("1234567890");
+	public void the_user_fill_the_all_details(DataTable CustomerDetails) {
+		List<String> cdl = CustomerDetails.asList(String.class);
+		System.out.println(cdl);
+
+		driver.findElement(By.xpath("//*[text()='Done']")).click();
+		driver.findElement(By.id("fname")).sendKeys(cdl.get(0));
+		driver.findElement(By.id("lname")).sendKeys(cdl.get(1));
+		driver.findElement(By.name("emailid")).sendKeys(cdl.get(2));
+		driver.findElement(By.name("addr")).sendKeys(cdl.get(3));
+		driver.findElement(By.name("telephoneno")).sendKeys(cdl.get(4));
 	}
 
 	@When("The user click the submit button")
 	public void the_user_click_the_submit_button() {
-	    driver.findElement(By.name("submit")).click();
+		driver.findElement(By.name("submit")).click();
 	}
 
 	@Then("The user get the CustomerID")
 	public void the_user_get_the_CustomerID() {
-	    WebElement text = driver.findElement(By.xpath("//table[@class='alt access']//tr/td[2]"));
-	    String str = text.getText();
-	    System.out.println("CustomerID"+":"+str);
+		WebElement text = driver.findElement(By.xpath("//table[@class='alt access']//tr/td[2]"));
+		String str = text.getText();
+		System.out.println("CustomerID" + ":" + str);
 	}
 
 	@Then("The user close the url")
 	public void the_user_close_the_url() {
-	    driver.close();
+		driver.close();
+	}
+	
+	@Given("The user Launch the url guru")
+	public void the_user_Launch_the_url_guru() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("http://demo.guru99.com/telecom/");
+		driver.manage().window().maximize();
 	}
 
-	
+	@Given("The user click the Add Tariff Plan")
+	public void the_user_click_the_Add_Tariff_Plan() {
+		driver.findElement(By.xpath("//*[text()='Add Tariff Plan']")).click();
+	}
+
+	@When("The user fill all details in tariff plan")
+	public void the_user_fill_all_details_in_tariff_plan(DataTable CustomerDetails) {
+		Map<String, String> cdam = CustomerDetails.asMap(String.class, String.class);
+		System.out.println(cdam);
+		
+		driver.findElement(By.id("rental1")).sendKeys(cdam.get("Monthly Rental"));
+		driver.findElement(By.id("local_minutes")).sendKeys(cdam.get("Free Local Minutes"));
+		driver.findElement(By.id("inter_minutes")).sendKeys(cdam.get("Free International Minutes"));
+		driver.findElement(By.id("sms_pack")).sendKeys(cdam.get("Free SMS Pack"));
+		driver.findElement(By.id("minutes_charges")).sendKeys(cdam.get("Local Per Minutes Charges"));
+		driver.findElement(By.id("inter_charges")).sendKeys(cdam.get("International Per Minutes Charges"));
+		driver.findElement(By.id("sms_charges")).sendKeys(cdam.get("SMS Per Charges"));
+	}
+
+	@When("The user click Submit button")
+	public void the_user_click_Submit_button() {
+		driver.findElement(By.name("submit")).click();
+	}
+
+	@Then("The user get the message congratulations")
+	public void the_user_get_the_message_congratulations() {
+		WebElement text = driver.findElement(By.xpath("//section[@id='main']//h2"));
+		String str = text.getText();
+		System.out.println(str);
+	}
+
 }
+	
--- src/test/resources/Features/customer.feature
+++ src/test/resources/Features/customer.feature
@@ -1,4 +1,4 @@
-Feature: Add Customer 
+Feature: Guru99 testing Demo page 
 
 Scenario: Add Customer "http://demo.guru99.com/telecom" 
 
@@ -6,10 +6,12 @@ Scenario: Add Customer "http://demo.guru99.com/telecom"
 	And The user click the Add customer 
 	
 	When The user fill the all details 
+		|Aro|Jacq|arojacq@gmail.com|Velachery|1234567890|
 	And The user click the submit button 
-	
 	Then The user get the CustomerID 
 	And The user close the url 
+	
+	
 	
 Scenario: Add Customer "http://demo.guru99.com/telecom" 
 
@@ -17,7 +19,27 @@ Scenario: Add Customer "http://demo.guru99.com/telecom"
 	And The user click the Add customer 
 	
 	When The user fill the all details 
+		|Aro|Jacq|arojacq@gmail.com|Velachery|1234567890|
 	And The user click the submit button 
-	
 	Then The user get the CustomerID 
 	And The user close the url 
+	
+	
+Scenario: Add Tariff Plan http://demo.guru99.com/telecom/ 
+
+
+	Given The user Launch the url guru 
+	And The user click the Add Tariff Plan 
+	
+	When The user fill all details in tariff plan 
+		|Monthly Rental|500|
+		|Free Local Minutes|250|
+		|Free International Minutes|2|
+		|Free SMS Pack|50|
+		|Local Per Minutes Charges|1|
+		|International Per Minutes Charges|50|
+		|SMS Per Charges|1|
+	And The user click Submit button 
+	
+	Then The user get the message congratulations 
+	
